de83b16f1c8788dd0ed7fada92cb5d014a3c3a34
JonesRod <batatajonesrodrigues@gmail.com>
2025-06-19 11:50:55 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/project_seg_lincon/', // Para GitHub Pages
+  base: './', // Para GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
